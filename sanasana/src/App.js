c34ee42d93bc54516f4338fc1c11c2689eb37d67
KarenWanyama3430 <you@example.com>
2024-06-29 02:40:21 +0300

    changes to the routing

--- sanasana/src/App.js
+++ sanasana/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './components/onboarding/signIn/SignIn';
+import Step1 from './components/onboarding/signUp/Step1';
+import Step2 from './components/onboarding/signUp/Step2';
 import './App.css';
-
 
 const App = () => {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/step1" component={Step1} />
+          <Route path="/step2" component={Step2} />
+          <Route path="/" component={SignIn} /> {/* Default route */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
