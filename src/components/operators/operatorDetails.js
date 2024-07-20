fb3799d89f279a256ea37eb6a13d11e541c5343a
perismuthoni <muthonimuriuki22@gmail.com>
2024-07-20 15:36:33 +0000

    adjusted operators screen functionality

--- src/components/operators/operatorDetails.js
+++ src/components/operators/operatorDetails.js
@@ -1,86 +1,92 @@
 /* eslint-disable react/prop-types */
-import { React, useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from "@mui/material";
+import { React, useState, useEffect } from "react";
+import { Grid, Card, CardContent, Typography } from '@mui/material';
 import Loader from '../loader'
 
-const PropCard = ({ selectedProperty }) => {
-  const baseURL = process.env.REACT_APP_BASE_URL;
+const PropCard = ({ selectedOperator }) => {
 
-  const [units, setUnits] = useState([]);
+
+
+
+
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const apiUrl = `${baseURL}/units`;
-    // to be corrected to dynamic
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setUnits(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, [baseURL]); // Empty dependency array ensures this effect runs only once when the component mounts
-
-  const propertyUnits = units.filter(
-    (unit) => unit["u_p_id"] === selectedProperty
-  );
-
-  console.log(selectedProperty);
+    if (selectedOperator) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+    }
+  }, [selectedOperator]);
+  
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Unitcode</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Type</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Start-date</TableCell>
-            <TableCell>End-date</TableCell>
-            <TableCell>Tenant </TableCell>
-            <TableCell>Tenant Mobile</TableCell>
-            <TableCell>Lease No </TableCell>
-          </TableRow>
-        </TableHead>
+    <>
+  
         {!loading && (
-          <>
-            <TableBody>
-              {propertyUnits.map((unit) => (
-                <TableRow key={unit.u_id}>
-                  <TableCell>{unit.u_name}</TableCell>
-                  <TableCell>{unit.u_description}</TableCell>
-                  <TableCell>{unit.u_type}</TableCell>
-                  <TableCell>{unit.u_status}</TableCell>
-                  <TableCell>{unit.l_start_date}</TableCell>
-                  <TableCell>{unit.l_end_date}</TableCell>
-                  <TableCell>{unit.l_lessee_name}</TableCell>
-                  <TableCell>{unit.l_code}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </>
+          
+              <div>
+                 {selectedOperator.map((Operator) => ( <>
+
+                  <Card style={{ height: '400px', width: '100%' }}>
+                  <Typography >Image</Typography>
+
+                  </Card>
+             
+        
+              <Card variant="outlined" style={{ marginTop: '20px' }}>
+             
+              <CardContent>
+  <Grid container spacing={2}>
+    <Grid item xs={6}>
+      <Typography >Name:</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <Typography>{Operator.o_name}</Typography>
+    </Grid>
+
+    <Grid item xs={6}>
+      <Typography>Status:</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <Typography>{Operator.o_status}</Typography>
+    </Grid>
+
+
+    <Grid item xs={6}>
+      <Typography>cum mileage:</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <Typography>{Operator.o_cum_mileage}</Typography>
+    </Grid>
+
+    <Grid item xs={6}>
+      <Typography>Phone:</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <Typography>{Operator.o_phone}</Typography>
+    </Grid>
+
+    <Grid item xs={6}>
+      <Typography>Expirence:</Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <Typography>{Operator.o_expirence}</Typography>
+    </Grid>
+
+  </Grid>
+</CardContent>
+                
+              </Card></>
+
+))}
+            </div>
+           
         )}
         
         { loading && ( <Loader/>
         )}
-      </Table>
-    </TableContainer>
+     
+    </>
   );
 };
 
