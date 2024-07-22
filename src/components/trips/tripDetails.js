5ef6a99384ceb3661c431accbb8ffe0b650ee1d9
perismuthoni <muthonimuriuki22@gmail.com>
2024-07-22 18:24:12 +0000

    driver view routing

--- src/components/trips/tripDetails.js
+++ src/components/trips/tripDetails.js
@@ -1,34 +1,20 @@
 /* eslint-disable react/prop-types */
 import { React, useState, useEffect } from "react";
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import {MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import { GoogleMap, LoadScript, Marker, Polyline, InfoWindow } from '@react-google-maps/api';
 import { Grid, Card, CardContent, Typography } from '@mui/material';
-import Loader from '../loader'
+import Loader from '../loader';
+import {APIProvider} from '@vis.gl/react-google-maps';
+import Reorder from "@mui/icons-material/Reorder";
+
+const mapContainerStyle = {
+  height: '400px',
+  width: '100%'
+};
 
 const PropCard = ({ selectedAsset }) => {
-  // Fix for marker icons not showing up in React-Leaflet
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
-});
-// const startLat = parseFloat(asset.t_start_lat) || 5.6037; 
-// const startLong = parseFloat(asset.t_start_long) || -0.1870;
-// const endLat = parseFloat(asset.t_end_lat) || 5.6037; 
-// const endLong = parseFloat(asset.t_end_long) || -0.1870;
-const startLat =  5.6037; 
-const startLong =  -0.1870;
-const endLat =  5.9037; 
-const endLong = -0.1170;
-
-const pathCoordinates = [
-  [startLat, startLong],
-  [endLat, endLong]
-];
-
+  const REACT_APP_GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     if (selectedAsset) {
       setLoading(false);
@@ -36,97 +22,117 @@ const pathCoordinates = [
       setLoading(true);
     }
   }, [selectedAsset]);
-  
+
+  const defaultCenter = { lat: 5.6037, lng: -0.1870 };
+
+
+  const infoWindowStyle = {
+    padding: '10px',
+    fontSize: '14px',
+    color: 'red'
+  };
 
   return (
     <>
-  
-        {!loading && (
-          
-              <div>
-                 {selectedAsset.map((asset) => ( <>
-                  
-              <MapContainer center={[5.6037, -0.1870]} zoom={13} style={{ height: '400px', width: '100%' }}>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-               <Marker position={[startLat, startLong]}>
-          <Popup>Start: {asset.a_license_plate}</Popup>
-        </Marker>
-        <Marker position={[endLat, endLong]}>
-          <Popup>End: {asset.a_license_plate}</Popup>
-        </Marker>
-        <Polyline positions={pathCoordinates} color="blue" />
-              </MapContainer>
-             
-        
-              <Card variant="outlined" style={{ marginTop: '20px' }}>
-             
-              <CardContent>
-  <Grid container spacing={2}>
-    <Grid item xs={6}>
-      <Typography >Asset Type:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_type}</Typography>
-    </Grid>
+      {!loading && (
 
-    <Grid item xs={6}>
-      <Typography>License Plate:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_license_plate}</Typography>
-    </Grid>
+<APIProvider apiKey={REACT_APP_GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
 
 
-    <Grid item xs={6}>
-      <Typography>Efficiency Rate:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_efficiency_rate}</Typography>
-    </Grid>
+        <div>
+          {selectedAsset.map((asset) => {
+            const startLat = parseFloat(asset.t_start_lat) || defaultCenter.lat;
+            const startLong = parseFloat(asset.t_start_long) || defaultCenter.lng;
+            const endLat = parseFloat(asset.t_end_lat) || defaultCenter.lat;
+            const endLong = parseFloat(asset.t_end_long) || defaultCenter.lng;
+            // const startLat = asset.t_start_lat;
+            // const startLong = asset.t_start_long;
+            // const endLat = asset.t_end_lat;
+            // const endLong = asset.t_end_long;
+            const isValidLatLng = (latLng) => {
+              return isFinite(latLng.lat) && isFinite(latLng.lng) && -90 <= latLng.lat && latLng.lat <= 90 && -180 <= latLng.lng && latLng.lng <= 180;
+            };
+            const center = isValidLatLng({ lat: startLat, lng: startLong })
+        ? { lat: startLat, lng: startLong }
+        : defaultCenter;
 
-    <Grid item xs={6}>
-      <Typography>Make & Model:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_make}:{asset.a_model}</Typography>
-    </Grid>
+          // Utility function to check for valid latitude and longitude values
 
-    <Grid item xs={6}>
-      <Typography>Engine:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_engine_size}</Typography>
-    </Grid>
 
-    <Grid item xs={6}>
-      <Typography>Value:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_value}</Typography>
-    </Grid>
 
-    <Grid item xs={6}>
-      <Typography>Acc Dep:</Typography>
-    </Grid>
-    <Grid item xs={6}>
-      <Typography>{asset.a_accumulated_dep}</Typography>
-    </Grid>
-  </Grid>
-</CardContent>
-                
-              </Card></>
+            console.log('center Coordinates:', center);
 
-))}
-            </div>
-           
-        )}
-        
-        { loading && ( <Loader/>
-        )}
-     
+            const pathCoordinates = [
+              { lat: startLat, lng: startLong },
+              { lat: endLat, lng: endLong }
+            ];
+
+            return (
+              <div key={asset.id}>
+
+
+
+<LoadScript googleMapsApiKey={REACT_APP_GOOGLE_MAPS_API_KEY}>
+                  <GoogleMap
+                    mapContainerStyle={mapContainerStyle}
+                      center={{ lat: startLat, lng: startLong }}
+                    zoom={13}
+                  >
+                    <Marker position={{ lat: startLat, lng: startLong }} icon={Reorder}>
+                      <InfoWindow position={{ lat: startLat, lng: startLong }}>
+                        <div style={infoWindowStyle}>Start: {asset.a_license_plate}</div>
+                      </InfoWindow>
+                    </Marker>
+                    <Marker position={{ lat: endLat, lng: endLong }} icon={Reorder}>
+                      <InfoWindow position={{ lat: endLat, lng: endLong }}>
+                        <div style={infoWindowStyle}>End: {asset.a_license_plate}</div>
+                      </InfoWindow>
+                    </Marker>
+                    <Polyline path={pathCoordinates} options={{ strokeColor: 'blue', strokeOpacity: 1.0, strokeWeight: 2 }} />
+                  </GoogleMap>
+                </LoadScript>
+
+                <Card>
+                  <CardContent>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <Typography>startLat: {startLat}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>startLong: {startLong}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>endLat: {endLat}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>endLong: {endLong}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>Status: {asset.a_status}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>Make & Model: {asset.a_make}:{asset.a_model}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>Engine: {asset.a_engine_size}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>Value: {asset.a_value}</Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography>Acc Dep: {asset.a_accumulated_dep}</Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </div>
+            );
+          })}
+        </div>
+        </APIProvider>
+      )}
+
+      {loading && <Loader />}
     </>
   );
 };
