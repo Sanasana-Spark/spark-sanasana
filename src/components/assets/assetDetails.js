9b4fd720448de85868b2cf9fce517db55f9ddbb5
perismuthoni <muthonimuriuki22@gmail.com>
2024-08-03 14:14:58 +0000

    dependacy resolved I think

--- src/components/assets/assetDetails.js
+++ src/components/assets/assetDetails.js
@@ -1,19 +1,11 @@
 /* eslint-disable react/prop-types */
 import { React, useState, useEffect } from "react";
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import {MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import { Grid, Card, CardContent, Typography } from '@mui/material';
-import Loader from '../loader'
+import Loader from '../loader';
+import Map from "../maps/singleTripMap";
 
 const PropCard = ({ selectedAsset }) => {
-  // Fix for marker icons not showing up in React-Leaflet
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
-});
 // const startLat = parseFloat(asset.t_start_lat) || 5.6037; 
 // const startLong = parseFloat(asset.t_start_long) || -0.1870;
 // const endLat = parseFloat(asset.t_end_lat) || 5.6037; 
@@ -22,6 +14,9 @@ const startLat =  5.6037;
 const startLong =  -0.1870;
 const endLat =  5.9037; 
 const endLong = -0.1170;
+
+const start = { lat: startLat, lng: startLong };
+const end = { lat: endLat, lng: endLong };
 
 const pathCoordinates = [
   [startLat, startLong],
@@ -45,20 +40,13 @@ const pathCoordinates = [
           
               <div>
                  {selectedAsset.map((asset) => ( <>
-                  
-              <MapContainer center={[5.6037, -0.1870]} zoom={13} style={{ height: '400px', width: '100%' }}>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-               <Marker position={[startLat, startLong]}>
-          <Popup>Start: {asset.a_license_plate}</Popup>
-        </Marker>
-        <Marker position={[endLat, endLong]}>
-          <Popup>End: {asset.a_license_plate}</Popup>
-        </Marker>
-        <Polyline positions={pathCoordinates} color="blue" />
-              </MapContainer>
+
+
+              <Map
+           startpoint= {start}
+            endpoint={end}
+            key={asset.id}
+           />
              
         
               <Card variant="outlined" style={{ marginTop: '20px' }}>
