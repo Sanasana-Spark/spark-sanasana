a1358a3951695f6bc2fb26852b03ba6fbc84c1c8
perismuthoni <muthonimuriuki22@gmail.com>
2025-03-16 18:36:18 +0300

    aapprove request fuel

---
+++ src/components/trips/trips_requested_table.js
@@ -0,0 +1,193 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Button,
+  TablePagination,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,TextField,
+  Typography
+} from "@mui/material";
+import { useAuthContext } from "../onboarding/authProvider";
+// import actionicon from "../../assets/actionicon.svg"
+
+const AssetsTable = ({ assets, onViewUnitsClick }) => {
+  const baseURL = process.env.REACT_APP_BASE_URL;
+  const { org_id, userId } = useAuthContext();
+  const [isDropdownOpen, setIsDropdownOpen] = useState({});
+  const [showFuelRequestForm, setShowFuelRequestForm] = useState(false);
+  const [selectedtrip, setSelectedTrip] = useState({});
+  const [ formData, setFormData] = useState({});
+  const [page, setPage] = useState(0); // Track the current page
+  const rowsPerPage = 7; // Number of records per page
+  const [, setLoading] = useState(false);
+  const [, setSuccess] = useState(null);
+  const [, setError] = useState(null);
+
+  const handleCellClick = (rowIndex) => {
+    setIsDropdownOpen((prevState) => ({
+      ...prevState,
+      [rowIndex]: !prevState[rowIndex], // Toggle the state for the specific asset id
+    }));
+    onViewUnitsClick(rowIndex);
+  };
+
+  const handleRequestClick = (trip) => {
+    setShowFuelRequestForm(true);
+    setSelectedTrip(trip);
+  };
+
+
+  const handleChange = (e) => {
+    if (!e || !e.target) return;
+
+  const { name, value } = e.target;
+
+  setFormData((prev) => ({
+    ...prev,
+    [name]: value,  // Dynamically update formData fields
+  }));
+
+  };
+
+  const handleSubmit = async () => {
+  console.log(formData);
+    const payload = {
+      t_id : selectedtrip.id,
+      t_actual_cost: formData.t_actual_cost,
+    }
+
+    try {
+      const response = await fetch(
+        `${baseURL}/trips/approve_request/${org_id}/${userId}/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to submit odometer reading");
+      }
+
+      setSuccess("Odometer reading submitted successfully!");
+      setShowFuelRequestForm(false);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const paginatedAssets = assets.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
+  return (
+    <TableContainer>
+      <Table stickyHeader aria-label="sticky table">
+        <TableHead>
+          <TableRow
+            backgroundColor="var(--secondary-bg-color)"
+            style={{ backgroundColor: "var(--secondary-bg-color)" }}
+          >
+            <TableCell>Details</TableCell>
+            <TableCell>LPO</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Operator</TableCell>
+            <TableCell>Vehicle</TableCell>
+            <TableCell>Origin</TableCell>
+            <TableCell>Destination</TableCell>
+            <TableCell>Distance</TableCell>
+            <TableCell>Fuel cost</TableCell>
+            <TableCell> Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* Render a TableRowItem for each asset in the assets array */}
+          {paginatedAssets.map((asset) => (
+            <TableRow
+              key={asset.id}
+              onMouseEnter={(e) =>
+                (e.currentTarget.style.backgroundColor =
+                  "var(--secondary-bg-color)")
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.backgroundColor = "var(--main-bg-color)")
+              }
+              sx={{ border: "none" }}
+            >
+              <TableCell onClick={() => handleCellClick(asset.id)}>
+                {!isDropdownOpen[asset.id] && <Button> View </Button>}
+
+                {isDropdownOpen[asset.id] && <Button>Back </Button>}
+              </TableCell>
+              <TableCell>{asset.t_type}</TableCell>
+              <TableCell>{asset.t_status}</TableCell>
+              <TableCell>{asset.o_name}</TableCell>
+              <TableCell>{asset.a_license_plate}</TableCell>
+              <TableCell>{asset.t_origin_place_query}</TableCell>
+              <TableCell>{asset.t_destination_place_query}</TableCell>
+              <TableCell>{asset.t_distance}</TableCell>
+              <TableCell>{asset.t_actual_cost}</TableCell>
+              <TableCell onClick={() => handleRequestClick(asset)}>
+                <Button> Approve </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        component="div"
+        count={assets.length} // Total number of records
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[]} // Disable option to change number of rows per page
+      />
+
+      <Dialog open={showFuelRequestForm} fullWidth>
+        <DialogTitle>Approve Request</DialogTitle>
+        <DialogContent>
+        <Typography> This is a fuel request by {selectedtrip.o_name} for asset {selectedtrip.a_license_plate} </Typography>
+    
+          <Typography>Distance: {selectedtrip.t_distance} </Typography>
+          <TextField
+            label="Amt disbursed"
+            variant="outlined"
+            type="number"
+            fullWidth
+            sx={{ marginTop: 2 }}
+            name="t_actual_cost"
+            value={formData.t_actual_cost}
+            onChange={(e) => handleChange(e)}
+          />
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={() => setShowFuelRequestForm(false)}>Cancel</Button>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </TableContainer>
+  );
+};
+
+export default AssetsTable;
