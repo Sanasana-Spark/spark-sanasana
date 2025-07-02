4f63487afcb5a2bbb0114546acf2bb4243b27984
perismuthoni <muthonimuriuki22@gmail.com>
2025-07-03 00:55:25 +0300

    fix refresh bugs

--- src/components/trips/tripDetails.js
+++ src/components/trips/tripDetails.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect} from "react";
-import { Grid, Card, CardContent, Typography } from "@mui/material";
+import { Grid, Card, CardContent, Typography, Box } from "@mui/material";
 import Loader from "../loader";
 import Map from "../maps/singleTripMap";
 //  import Map1 from "../maps/newmap";
@@ -43,79 +43,76 @@ const PropCard = ({ selectedAsset }) => {
 
 
         return (
-          <div key={asset.id}>
-{/* 
-           <Map
-           origin= {asset.t_origin_place_query}
-           destination={asset.t_destination_place_query}
+          <Box
             key={asset.id}
-            center = {start}
-           /> */}
+            sx={{
+              backgroundColor: "#F9FAFB", // Light gray background
+              boxShadow: 2, // Adds a slight shadow
+              borderRadius: 2, // Rounded corners
+              padding: 2, // Adds spacing inside
+              marginBottom: 2, // Adds spacing between sections
+            }}
+          >
+            <Typography variant="h6" fontWeight="bold">
+              {asset.a_license_plate} ({asset.a_make} - {asset.a_model})
+            </Typography>
 
-<Map
-           origin= {origin}
-           destination={destination}
-            key={asset.id}
-            center = {start}
-           />
-
-{/* <Map1
-           origin= {start}
-           destination={end}
-           key={asset.id}
-           center = {start}
-           />  */}
-
-
-
+            <Box sx={{ marginBottom: 2 }}>
+              <Map
+                origin={origin}
+                destination={destination}
+                key={asset.id}
+                center={start}
+              />
+            </Box>
 
             <Card>
               <CardContent>
                 <Grid container spacing={2}>
-                  {/* <Grid item xs={12}>
-                    <Typography>start coordinates: {JSON.stringify(start)}</Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography>end: {JSON.stringify(end)}</Typography>
-                  </Grid> */}
-                  <Grid item xs={12}>
-                    <Typography> {asset.a_license_plate} ({asset.a_make}-{asset.a_model}) </Typography>
-                  </Grid>
-   
-   <Grid item xs={12 } sm={6}>
-                    <Typography>LPO: CL-{asset.id}</Typography>
-                    
-                  </Grid>
+                
 
-                  <Grid item xs={12} sm={6} >
+                 
+
+                  <Grid item xs={12} sm={6}>
                     <Typography>start: {asset.t_origin_place_query}</Typography>
                   </Grid>
 
-                  <Grid item xs={12} sm={6} >
-                    <Typography>end: {asset.t_destination_place_query}</Typography>
+                  <Grid item xs={12} sm={6}>
+                    <Typography>
+                      end: {asset.t_destination_place_query}
+                    </Typography>
                   </Grid>
-                   
-                  <Grid item xs={12} sm={6} >
+
+                  <Grid item xs={12} sm={6}>
                     <Typography>Status: {asset.t_status}</Typography>
                   </Grid>
-                  <Grid item xs={12} sm={6} >
+                  <Grid item xs={12} sm={6}>
                     <Typography>Driver: {asset.o_name}</Typography>
                   </Grid>
-                  
-                  <Grid item xs={12} sm={6} >
+
+                  <Grid item xs={12} sm={6}>
                     <Typography>Distance: {asset.t_distance}</Typography>
                   </Grid>
-                  <Grid item xs={12} sm={6} >
+                  <Grid item xs={12} sm={6}>
                     <Typography>Tonnage: {asset.t_load}</Typography>
                   </Grid>
-                  <Grid item xs={12} sm={6} >
-                    <Typography>Fuel: {asset.t_actual_fuel}(Ltrs):Ksh{asset.t_actual_cost}</Typography>
+                  <Grid item xs={12} sm={6}>
+                    <Typography>
+                      Fuel: {asset.t_actual_fuel}(Ltrs)
+                    </Typography>
                   </Grid>
-               
+                  <Grid item xs={12} sm={6}>
+                    <Typography>
+                      Fuel:Ksh{asset.t_actual_cost}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <Typography>Desc: {asset.t_type}</Typography>
+                  </Grid>
                 </Grid>
               </CardContent>
             </Card>
-          </div>
+          </Box>
         );
       })}
     </div>
