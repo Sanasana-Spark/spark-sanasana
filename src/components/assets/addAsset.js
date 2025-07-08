04c37ef93dc72591a3ac9221971a3fcefaf59878
perismuthoni <muthonimuriuki22@gmail.com>
2025-07-09 01:36:36 +0300

    asset form

--- src/components/assets/addAsset.js
+++ src/components/assets/addAsset.js
@@ -1,52 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from "react";
 // import PropTypes from "prop-types";
-import { Modal, Box, Typography, TextField, Button, MenuItem, IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select
+} from "@mui/material";
 
 const AddAssetForm = ({ onSubmit, onCancel, open }) => {
-  const baseURL = process.env.REACT_APP_BASE_URL
-  const [loading, setLoading] = useState(true);
-  const [statusOptions, setStatusOptions] = useState([]);
+  const [saving, setSaving] = useState(false);
 
-  const years = Array.from(new Array(50), (val, index) =>
-    new Date().getFullYear() - index
+  const years = Array.from(
+    new Array(50),
+    (val, index) => new Date().getFullYear() - index
   );
-
-  useEffect(() => {
-    if(open){
-      const apiUrl = `${baseURL}/assets/status`;
-      fetch(apiUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setStatusOptions(data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          setLoading(false);
-        });
-  }
-},[baseURL, open]);
-
-
 
 
   //   const classes = useStyles();
   const [formData, setFormData] = useState({
-    a_make: "",
-    a_model: "",
-    a_year: "",
-    a_fuel_type: "Diesel",
-    a_tank_size: 100,
-    a_displacement: "",
-    a_mileage: "",
+    a_make: null,
+    a_model: null,
+    a_year: null,
+    a_fuel_type: null,
+    a_tank_size: null,
+    a_displacement: null,
+    a_mileage: null,
     a_horsepower: null,
-    a_acceleration: null,
+    a_acceleration: 2,
     a_insurance_expiry: null,
     a_license_plate: null,
     a_status: "Active",
@@ -55,7 +43,6 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
     a_attachment1: null,
     a_attachment2: null,
     a_attachment3: null,
-
   });
 
   const handleChange = (e) => {
@@ -72,124 +59,271 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
     // Optionally, you can reset the form after submission
     setFormData({
       a_status: "Active",
-      a_fuel_type:"Diesel"
-
+      a_fuel_type: "Diesel",
     });
   };
 
-  const handleFileChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.files[0],
-    });
-  };
-
-  if (loading) {
-    return <></>;
-  }
+  //   const handleFileChange = (e) => {
+  //   const file = e.target.files[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setFormData({
+  //         ...formData,
+  //           "a_attachment1": {
+  //               name: file.name,
+  //               type: file.type,
+  //               size: file.size,
+  //               data: reader.result.split(",")[1], // Extract the base64 string
+  //         }, // base64 string
+  //       });
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
 
 
   return (
-    <Modal open={open} onClose={onCancel} sx={{ zIndex: 100}} >
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: 500,
-          maxHeight: "80vh",
-          overflowY: "auto",
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-          borderRadius: 2,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            position: "sticky",
-            top: 0,
-            bgcolor: "background.paper",
-            zIndex: 10,
-            pb: 1,
-          }}
-        >
-          <Typography variant="h6">Add Vehicle</Typography>
-          <IconButton onClick={onCancel}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
+    <Dialog open={open} onClose={onCancel} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Add Vehicle</DialogTitle>
+      <DialogContent>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  required
+                  label="Make(eg: Toyota)"
+                  name="a_make"
+                  value={formData.a_make}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  label="Model(eg: Corolla)"
+                  name="a_model"
+                  value={formData.a_model}
+                  onChange={handleChange}
+                />
+              </Grid>
 
-
-        {/* Section 1 */}
-        <Typography variant="subtitle1" sx={{ mt: 2, fontWeight: 'bold' }}>Vehicle Details</Typography>
-        <TextField required label="Make(eg: mercedes)" name="a_make" fullWidth margin="dense" onChange={handleChange} />
-        <TextField required label="Model(eg: zetros)" name="a_model" fullWidth margin="dense" onChange={handleChange} />
-        <TextField  required select label="Year" name="a_year" fullWidth margin="dense" onChange={handleChange}>
-          {years.map((year) => (
-            <MenuItem key={year} value={year}>
-              {year}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField required select label="Fuel Type" name="a_fuel_type" fullWidth margin="dense" onChange={handleChange}>
-          <MenuItem value="Petrol">Petrol</MenuItem>
-          <MenuItem value="Diesel">Diesel</MenuItem>
-        </TextField>
-        <TextField required label="Tank Size" name="a_tank_size" fullWidth margin="dense" onChange={handleChange} />
-        <TextField required label="Displacement/cc" name="a_displacement" fullWidth margin="dense" onChange={handleChange} />
-        <TextField required label="Mileage" name="a_mileage" fullWidth margin="dense" onChange={handleChange} />
-        <TextField label="Horsepower" name="a_horsepower" fullWidth margin="dense" onChange={handleChange} />
-        <TextField label="Acceleration" name="a_acceleration" fullWidth margin="dense" onChange={handleChange} />
-        <TextField required label="Reg No" name="a_license_plate" fullWidth margin="dense" onChange={handleChange} />
-        
-        <TextField label="Status" name="a_status" fullWidth margin="dense" onChange={handleChange} > 
- {statusOptions.map((status) => (
-                      <MenuItem key={status.id} value={status.s_name}>
-                        {status.s_name}
+              <Grid item xs={12} sm={6}>
+                <FormControl required fullWidth>
+                  <InputLabel id="asset-label">Year</InputLabel>
+                  <Select
+                    required
+                    fullWidth
+                    labelId="asset-label"
+                    id="a_year"
+                    label="a_year"
+                    name="a_year"
+                    value={formData.a_year}
+                    onChange={handleChange}
+                  >
+                    {years.map((year) => (
+                      <MenuItem key={year} value={year}>
+                        {year}
                       </MenuItem>
-                    ))} 
+                    ))}
+                  </Select>{" "}
+                </FormControl>
+              </Grid>
 
-        </TextField>
-        <TextField required label="Insurance Expiry Date"  name="a_insurance_expiry"
-                          type="date"
-                          onChange={(date) => setFormData({ ...formData, insuranceExpiryDate: date })}
-          renderInput={(params) => <TextField {...params} fullWidth margin="dense" />}
-                        />
+              <Grid item xs={12} sm={6}>
+                <FormControl required fullWidth>
+                  <InputLabel id="a_fuel_type">Fuel Type</InputLabel>
+                  <Select
+                    required
+                    labelId="a_fuel_type"
+                    id="a_fuel_type"
+                    label="a_fuel_type"
+                    name="a_fuel_type"
+                    value={formData.a_fuel_type}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="Petrol">Petrol</MenuItem>
+                    <MenuItem value="Diesel">Diesel</MenuItem>
+                  </Select>{" "}
+                </FormControl>
+              </Grid>
 
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  type="number"
+                  label="Tank Size(in litres)"
+                  name="a_tank_size"
+                  value={formData.a_tank_size}
+                  onChange={handleChange}
+                />
+              </Grid>
 
-        {/* Section 2 */}
-        <Typography variant="subtitle1" sx={{ mt: 2, fontWeight: 'bold' }}>
-          Valuation
-        </Typography>
-        <Typography variant="body2" sx={{ fontSize: "10px" }}>
-          This is to help calculation depreciation of your vehicles
-        </Typography>
-        <TextField label="Cost" name="a_cost" fullWidth margin="dense" onChange={handleChange} />
-        <TextField label="Valuation" name="a_value" fullWidth margin="dense" onChange={handleChange} />
+               <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  type="number"
+                  label="Mileage(in KM)"
+                  name="a_mileage"
+                  value={formData.a_mileage}
+                  onChange={handleChange}
+                />
+              </Grid>
 
-        {/* Section 3 */}
-        <Typography variant="subtitle1" sx={{ mt: 2, fontWeight: 'bold' }}>
-          Attachments
-        </Typography>
-        <Typography variant="body2" sx={{ fontSize: "10px" }} >
-          Upload is necessary attachment for reference
-        </Typography>
-        <input type="file" name="a_attachment1" onChange={handleFileChange} />
-        <input type="file" name="a_attachment2" onChange={handleFileChange} />
-        <input type="file" name="a_attachment3" onChange={handleFileChange} />
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  type="number"
+                  label="Displacement(2500cc)"
+                  name="a_displacement"
+                  value={formData.a_displacement}
+                  onChange={handleChange}
+                />
+              </Grid>
 
-        <Button variant="contained" color="primary" sx={{ mt: 2 }} fullWidth onClick={handleSubmit}>
-          Submit
-        </Button>
-      </Box>
-    </Modal>
+             
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  type="number"
+                  label="Acceleration((m/s²)"
+                  name="a_acceleration"
+                  value={formData.a_acceleration}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  label="Reg No/ Plate No"
+                  name="a_license_plate"
+                  value={formData.a_license_plate}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  required
+                  id="status-label"
+                  label="Insurance Expiry Date"
+                  name="a_insurance_expiry"
+                  type="date"
+                  value={formData.a_insurance_expiry}
+                  onChange={handleChange}
+                  InputLabelProps={{ shrink: true }}
+                />
+              </Grid>
+              {/* 
+             onChange={(date) =>
+                  setFormData({ ...formData, insuranceExpiryDate: date })
+                } */}
+
+              {/* Section 2 */}
+
+              <Grid item xs={12} sm={12}>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ mt: 2, fontWeight: "bold" }}
+                >
+                  Valuation
+                </Typography>
+                <Typography variant="body2" sx={{ fontSize: "10px" }}>
+                  This is to help calculate depreciation of your vehicles
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  type="number"
+                  label="Acquisition Cost"
+                  name="a_cost"
+                  value={formData.a_cost}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  type="number"
+                  label="current Valuation"
+                  name="a_value"
+                  value={formData.a_value}
+                  onChange={handleChange}
+                />
+              </Grid>
+{/* 
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <FormLabel>
+                    Attachments{" "}
+                    <Typography variant="body2" sx={{ fontSize: "10px" }}>
+                      Upload is necessary attachment for reference
+                    </Typography>{" "}
+                  </FormLabel>
+                  <input
+                    accept="application/pdf, image/*"
+                    type="file"
+                    onChange={handleFileChange}
+                    name="a_attachment1"
+                  />
+                  {/* <input
+                    accept="application/pdf, image/*"
+                    type="file"
+                    name="a_attachment2"
+                    onChange={handleFileChange}
+                  />
+                  <input
+                    accept="application/pdf, image/*"
+                    type="file"
+                    name="a_attachment3"
+                    onChange={handleFileChange}
+                  /> 
+                </FormControl>
+              </Grid> */}
+
+            </Grid>
+
+            <DialogActions>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  onCancel();
+                  setSaving(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  backgroundColor: "var(--secondary-color)",
+                  color: "white",
+                }}
+                disabled={saving}
+              >
+                {saving ? "submitting..." : "Submit"}
+              </Button>
+            </DialogActions>
+          </form>
+       
+      </DialogContent>
+    </Dialog>
   );
-
 };
 
 export default AddAssetForm;
