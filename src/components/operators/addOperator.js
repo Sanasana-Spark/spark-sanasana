620c3926f5f3ef7eb75aade83329c65c2a51aca0
perismuthoni <muthonimuriuki22@gmail.com>
2025-07-16 00:18:42 +0300

    drop props and empty strings

--- src/components/operators/addOperator.js
+++ src/components/operators/addOperator.js
@@ -1,47 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Grid, FormControl, FormLabel, Select, MenuItem, InputLabel } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Grid, FormControl, Select, MenuItem, InputLabel } from '@mui/material';
 import { useAuthContext } from '../onboarding/authProvider';
 
 const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 	const baseURL = process.env.REACT_APP_BASE_URL;
 	const { user_id, org_id } = useAuthContext();
-
-	const [statusOptions, setStatusOptions] = useState([]);
+	const [saving, setSaving] = useState(false);
 	const [assetOptions, setAssetOptions] = useState([]);
 	const [loading, setLoading] = useState(true);
-	const [isSubmitting, setIsSubmitting] = useState(false);
 
 	const [operator, setOperator] = useState({
 		o_status: 'Active',
 		o_cum_mileage: 0,
 		o_role: 'Driver',
 		o_class: 'Class B',
-		o_email: '',
-		o_phone: '',
+		o_email: null,
+		o_phone: null,
 	});
 
 	const [errors, setErrors] = useState({
-		o_email: '',
-		o_phone: '',
+		o_email: null,
+		o_phone: null,
 	});
 
-	useEffect(() => {
-		const apiUrl = `${baseURL}/operators/status`;
-		fetch(apiUrl)
-			.then(response => {
-				if (!response.ok) throw new Error('Network response was not ok');
-				return response.json();
-			})
-			.then(data => {
-				setStatusOptions(data);
-				setLoading(false);
-			})
-			.catch(error => {
-				console.error('Error fetching status:', error);
-				setLoading(false);
-			});
-	}, [baseURL]);
 
 	useEffect(() => {
 		if (org_id && user_id) {
@@ -60,7 +41,9 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 					setLoading(false);
 				});
 		}
-	}, [baseURL, org_id, user_id]);
+	},
+	// eslint-disable-next-line
+	 [org_id, user_id]);
 
 	const handleChange = e => {
 		const { name, value } = e.target;
@@ -88,26 +71,26 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		setIsSubmitting(true);
+		setSaving(true);
 		onSubmit(operator);
 	};
 
-	const handleFileChange = e => {
-		const { name, files } = e.target;
-		const file = files[0];
+	// const handleFileChange = e => {
+	// 	const { name, files } = e.target;
+	// 	const file = files[0];
 
-		setOperator(prev => ({
-			...prev,
-			[name]: file,
-		}));
-	};
+	// 	setOperator(prev => ({
+	// 		...prev,
+	// 		[name]: file,
+	// 	}));
+	// };
 
 	return (
 		<Dialog open={open} onClose={onCancel} aria-labelledby='form-dialog-title'>
 			<DialogTitle id='form-dialog-title'>Add Driver</DialogTitle>
 			<DialogContent>
 				<form onSubmit={handleSubmit}>
-					<Grid container spacing={2}>
+					<Grid container spacing={2} paddingTop={1}>
 					
 						{/* fix if you want to add an image upload feature */}
 						{/* <Grid item xs={12}>
@@ -124,14 +107,37 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 							<TextField fullWidth required label='Email' name='o_email' type='email' value={operator.o_email} onChange={handleChange} error={!!errors.o_email} helperText={errors.o_email} />
 						</Grid>
 						<Grid item xs={12} sm={6}>
-							<TextField fullWidth required label='Phone' name='o_phone' value={operator.o_phone} onChange={handleChange} error={!!errors.o_phone} helperText={errors.o_phone} />
+							<TextField fullWidth required  label='Phone' name='o_phone' value={operator.o_phone} onChange={handleChange} error={!!errors.o_phone} helperText={errors.o_phone} />
 						</Grid>
-						<Grid item xs={12} sm={6}>
-							<TextField fullWidth label='National ID' name='o_national_id' value={operator.o_national_id} onChange={handleChange} />
+
+							<Grid item xs={12} sm={6}>
+							<FormControl required fullWidth>
+								<InputLabel id='asset-label'>Assigned Vehicle</InputLabel>
+								<Select
+									labelId='asset-label'
+									name='o_assigned_asset'
+									value={operator.o_assigned_asset || ''}
+									onChange={handleChange}
+									label='Assigned Vehicle'
+									disabled={loading}
+								>
+									{loading ? (
+										<MenuItem disabled>Loading vehicles...</MenuItem>
+									) : assetOptions.length > 0 ? (
+										assetOptions.map(asset => (
+											<MenuItem key={asset.id} value={asset.id}>
+												{asset.a_license_plate} - {asset.a_status}
+											</MenuItem>
+										))
+									) : (
+										<MenuItem disabled>No vehicles found</MenuItem>
+									)}
+								</Select>
+							</FormControl>
 						</Grid>
-						<Grid item xs={12} sm={6}>
-							<TextField fullWidth label='License ID' name='o_lincense_id' value={operator.o_lincense_id} onChange={handleChange} />
-						</Grid>
+
+
+					
 
 						<Grid item xs={12} sm={6}>
 							<FormControl required fullWidth>
@@ -159,20 +165,14 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 							<TextField fullWidth required label='License Expiry' name='o_lincense_expiry' type='date' value={operator.o_lincense_expiry || ''} onChange={handleChange} InputLabelProps={{ shrink: true }} />
 						</Grid>
 
-						<Grid item xs={12} sm={6}>
-							<FormControl required fullWidth>
-								<InputLabel id='asset-label'>Assigned Vehicle</InputLabel>
-								<Select labelId='asset-label' name='o_assigned_asset' value={operator.o_assigned_asset} onChange={handleChange} label='Assigned Vehicle'>
-									{assetOptions.length > 0
-										? assetOptions.map(asset => (
-												<MenuItem key={asset.id} value={asset.id}>
-													{asset.a_license_plate} - {asset.a_status}
-												</MenuItem>
-										  ))
-										: loading && <MenuItem disabled>Loading vehicles...</MenuItem>}
-								</Select>
-							</FormControl>
+							<Grid item xs={12} sm={6}>
+							<TextField fullWidth label='National ID' name='o_national_id' value={operator.o_national_id} onChange={handleChange} />
 						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField fullWidth label='License ID' name='o_lincense_id' value={operator.o_lincense_id} onChange={handleChange} />
+						</Grid>
+
+					
 
 						{/* <Grid item xs={12} sm={6}>
 							<FormControl required fullWidth>
@@ -198,11 +198,15 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 					</Grid>
 
 					<DialogActions>
-						<Button variant='contained' color='primary' onClick={onCancel}>
+						<Button variant='contained' color='primary' onClick={() => {
+                  onCancel();
+                  setSaving(false);
+                }}
+				>
 							Cancel
 						</Button>
-						<Button type='submit' variant='contained' disabled={isSubmitting} sx={{ backgroundColor: 'var(--secondary-color)', color: 'white' }}>
-							{isSubmitting ? 'Submitting...' : 'Submit'}
+						<Button type='submit' variant='contained' disabled={saving} sx={{ backgroundColor: 'var(--secondary-color)', color: 'white' }}>
+							{saving ? 'Submitting...' : 'Submit'}
 						</Button>
 					</DialogActions>
 				</form>
@@ -211,10 +215,5 @@ const AddAssetForm = ({ onSubmit, onCancel, open }) => {
 	);
 };
 
-AddAssetForm.propTypes = {
-	onSubmit: PropTypes.func.isRequired,
-	onCancel: PropTypes.func.isRequired,
-	open: PropTypes.bool.isRequired,
-};
 
 export default AddAssetForm;
