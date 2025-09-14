c0ef108bd00764bb9c17092c9b79b3aeee024c07
perismuthoni <muthonimuriuki22@gmail.com>
2025-09-14 23:58:06 +0300

    adjust endpoint

--- src/pages/fuel.js
+++ src/pages/fuel.js
@@ -21,8 +21,7 @@ const Fuel = () => {
 	const [filteredEntries, setFilteredEntries] = useState([]);
 
 	useEffect(() => {
-		const apiUrl = `${baseURL}/fuel/`;
-		apiFetch(apiUrl, { method: 'GET' })
+		apiFetch(`${baseURL}/fuel/`, { method: 'GET' })
 			.then(response => {
 				if (!response.ok) throw new Error('Network response was not ok');
 				return response.json();
